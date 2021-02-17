1628edcbb5d037203a00c2168403f157f0e416f3
Abdeldjallil Hadji <ncdjilou09@gmail.com>
2021-02-18 00:40:31 +0100

    style header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Button from "@material-ui/core/Button";
+import emoji from "./img/usa-flag.png";
+import "./style.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div className="logo">
+          <h1>Logo</h1>
+        </div>
+        <div className="language">
+          <img className="emoji" src={emoji} alt="emoji" />
+          <h3>English</h3>
+        </div>
+      </header>
     </div>
   );
 }
